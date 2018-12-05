dc4b0a40c58bdcd425a67e952b63c10b1763a1ab
Paul Trin-Dinh <paultrindinh@icloud.com>
2018-12-05 15:45:29 +0100

    Added all necessary pages with minimal views, changed seed accordingly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @cities = City.all
+    @places = Place.all
     @events = Event.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
+  get 'cities/index'
+  get 'cities/show'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
