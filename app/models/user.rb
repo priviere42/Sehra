65e8110b3bbd4eec36e7d2ada407f2ca4362cd06
Blud7 <paul.riviere.pr@gmail.com>
2018-12-05 16:22:54 +0100

    added f.name and l.name fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	def phone_number_required?
+	   false
+	end
+	 
+	def phone_number_changed?
+	   false
+	end
+ 
 end
