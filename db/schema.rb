c8ba6c687bdf6818f8c56a06bd45febb232526a2
Paul Trin-Dinh <paultrindinh@icloud.com>
2018-12-05 16:37:25 +0100

    Fixed errors

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2018_12_05_113623) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
