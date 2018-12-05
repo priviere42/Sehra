18a71ab2b77c67e018feebd1958a0e4dae18e4f4
Paul Trin-Dinh <paultrindinh@icloud.com>
2018-12-05 11:26:19 +0100

    Added seed, images and home controller with index view showing each city.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.create!(name: "Bordeaux" , description: "La Belle Endormie" ,image_url: "/assets/city/bordeaux.jpg")
+City.create!(name: "Lyon" , description: "La Capitale des Gaules" ,image_url: "/assets/city/lyon.jpg")
+City.create!(name: "Marseille" , description: "La Cité Phocéenne" ,image_url: "/assets/city/marseille.jpg")
+City.create!(name: "Nice" , description: "Nissa la Bella" ,image_url: "/assets/city/nice.jpg")
+City.create!(name: "Paris" , description: "Paname, la Capitale" ,image_url: "/assets/city/paris.jpg")
+City.create!(name: "Toulouse" , description: "La Ville Rose" ,image_url: "/assets/city/toulouse.jpg")
