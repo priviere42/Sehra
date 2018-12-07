884205d3c206a92cdf0ca6c7e54f85b9e5374d60
Blud7 <paul.riviere.pr@gmail.com>
2018-12-07 18:52:55 +0100

    edited credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :mailjet
 
   # Show full error reports.
   config.consider_all_requests_local = true
